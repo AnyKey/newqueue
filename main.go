a273a3819d49e9501114f72744c3118d49317484
spoprotskiy <spoprotskiy@zuzex.com>
2021-04-16 16:26:35 +0300

    123

--- main.go
+++ main.go
@@ -1,37 +1,70 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
+	"github.com/streadway/amqp"
 	"log"
 	"os"
-	"strconv"
-	"time"
 )
 
 func main() {
-	file, err := os.OpenFile("/home/senpai/docker-elk/logs/ololo.log",os.O_CREATE | os.O_RDWR | os.O_TRUNC, 0755)
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	msgs := getQueue(ch)
+
+	forever := make(chan bool)
+
+	file, err := os.OpenFile("/home/redhat/docker-elk/logs/main.log", os.O_CREATE|os.O_RDWR|os.O_TRUNC, 0755)
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 
-	log.SetOutput(file)
-
-	type LogMessage struct {
-		Time time.Time `json:"time"`
-		Message string `json:"message"`
-	}
-
-	i:=0
-	for range time.Tick(time.Second) {
-		i++
-		msg := LogMessage{
-			Time:    time.Now(),
-			Message: "tick number : " + strconv.Itoa(i),
+	go func() {
+		for d := range msgs {
+			bytes := d.Body
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Fprintln(file, string(bytes))
 		}
-		bytes, _ :=json.Marshal(msg)
+	}()
+	fmt.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
 
-		fmt.Println(string(bytes))
-		log.Println(string(bytes))
+func getQueue(ch *amqp.Channel) <-chan amqp.Delivery {
+	q, err := ch.QueueDeclare(
+		"main_queue", // name
+		false,        // durable
+		false,        // delete when unused
+		false,        // exclusive
+		false,        // no-wait
+		nil,          // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+	return msgs
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Printf("%s: %s", msg, err)
 	}
 }
